02e3f23a2baa2bde588a63bb3c0803a85c1b1782
TheAmazingSui <youssef7kkader@gmail.com>
2025-10-11 19:03:18 +0200

    small issue fixed in Makefile

--- SOURCES-JVN/SOURCES-JVN/src/jvn/Coordinator/Coordinator.java
+++ SOURCES-JVN/SOURCES-JVN/src/jvn/Coordinator/Coordinator.java
@@ -2,29 +2,32 @@ package jvn.Coordinator;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
-
-import jvn.Utils.JvnRemoteCoord;
 
 public class Coordinator {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello from Coordinator!");
         System.out.println("Creating coordinator interface...");
         JvnCoordImpl jvnCoordImpl = new JvnCoordImpl();
-        JvnRemoteCoord jvn_stub = (JvnRemoteCoord) UnicastRemoteObject.exportObject(jvnCoordImpl, 0);
-        Registry registry = null;
-        if(args.length > 0 )
-        {
-            registry = LocateRegistry.getRegistry(Integer.parseInt(args[0]));
-
+        
+        // Create the registry programmatically to ensure proper classpath
+        Registry registry;
+        int port = (args.length > 0) ? Integer.parseInt(args[0]) : 1099;
+        
+        try {
+            // Try to create a new registry
+            registry = LocateRegistry.createRegistry(port);
+            System.out.println("Created new RMI registry on port " + port);
+        } catch (Exception e) {
+            // If registry already exists, get reference to it
+            registry = LocateRegistry.getRegistry(port);
+            System.out.println("Using existing RMI registry on port " + port);
         }
-        else{
-            registry = LocateRegistry.getRegistry();
-        }
 
-        registry.rebind("Javanaise", jvn_stub);
+        registry.rebind("Javanaise", jvnCoordImpl);
 
         System.out.println("Coordinator ready!");
         
+        // Keep the coordinator running
+        System.out.println("Coordinator is running... Press Ctrl+C to stop.");
     }
 }
